8df30d48ec78dc0bc35427fdde41f17669b01267
lee2sman <leetusman@gmail.com>
2016-10-31 00:16:14 -0700

    set nvim as editor in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 # Add dotfiles bin programs to my PATH in FISH instead of in bash_profile or bashrc
 set PATH /Users/2sman/dotfiles/bin $PATH
 
+#Make Nvim the default editor
+set --export EDITOR "nvim"
+
 # fisherman confi info
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
