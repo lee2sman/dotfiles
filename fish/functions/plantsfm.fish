7c76a2d60e714b2eef491797361d8953799c7baf
lee2sman <leetusman@gmail.com>
2017-01-10 00:10:51 -0800

    added whitenoise and plantsfm functions

---
+++ fish/functions/plantsfm.fish
@@ -0,0 +1,3 @@
+function plantsfm
+	mplayer http://104.174.98.72:8000/listen
+end
---
+++ fish/functions/whitenoise.fish
@@ -0,0 +1,3 @@
+function whitenoise
+	play -n -c1 synth whitenoise lowpass -1 120 lowpass -1 120 lowpass -1 120 gain +15
+end
