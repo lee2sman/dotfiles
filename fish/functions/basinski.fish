b3c0b2062a46bfdbb57eb2731aa0f161b2ef4189
lee2sman <leetusman@gmail.com>
2017-07-10 19:10:01 -0700

    audio and roguelike aliases added

---
+++ fish/functions/basinski.fish
@@ -0,0 +1,3 @@
+function basinski
+	mplayer -loop 0 ~/Music/basinski/Basinski_South_of_Sunset_Bootleg.m4a
+end
---
+++ fish/functions/roguelikes.fish
@@ -0,0 +1,13 @@
+function roguelikes
+	echo "Roguelikes installed"
+echo "===================="
+echo
+echo "1) Rogue"
+echo "2) NetHack"
+echo "3) Brogue"
+echo "4) Dungeon Crawl Stone Soup"
+echo "5) Cataclysm"
+echo "6) Angband"
+echo "7) Omega"
+echo "8) Avanor"
+end
