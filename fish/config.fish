c6cc182caab82f9507e94e5a3a5ccaf5054684db
lee2sman <leetusman@gmail.com>
2017-07-10 19:08:47 -0700

    fixed config file after greeting broke fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,6 @@ set PATH /Users/2sman/dotfiles/bin $PATH
 
 #Make Nvim the default editor
 set --export EDITOR "nvim"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/2sman/exec -1 /usr/local/bin/fish/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/2sman/exec -1 /usr/local/bin/fish/google-cloud-sdk/path.fish.inc'; else; . '/Users/2sman/exec -1 /usr/local/bin/fish/google-cloud-sdk/path.fish.inc'; end; end
