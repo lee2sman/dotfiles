a140b610085c36e403c3d9d41037984576940b21
lee2sman <leetusman@gmail.com>
2022-01-05 02:07:45 -0500

    copy should work on WSL and non now

--- .config/fish/functions/copy.fish
+++ .config/fish/functions/copy.fish
@@ -1,3 +1,10 @@
 function copy --description 'copies file to clipboard'
-cat $argv | xclip -selection clipboard
+
+	if test (uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') 
+	    # This is Windows WSL baby!
+	    cat $argv | clip.exe
+	else
+	    # Not Windows
+	    cat $argv | xclip -selection clipboard
+	end
 end
