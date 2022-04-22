0068d0a25de83fe24678a7a2892bc5b966b03f49
lee2sman <leetusman@gmail.com>
2022-04-22 02:45:51 -0400

    update backitup.fish

--- config/fish/functions/backitup.fish
+++ config/fish/functions/backitup.fish
@@ -1,11 +1,19 @@
 function backitup
-if test $hostname = '3sman'
-  echo backing up xps13 computer
-  echo "this will take a few minutes..."
-  sleep 2
-  rsync -av /home/lee/ /media/lee/backup/xps13/
-  echo ""
-  echo "...complete"
-end
+  if test $hostname = '3sman'
+    echo backing up xps13 computer
+    echo "this will take a few minutes..."
+    sleep 2
+    rsync -av /home/lee/ /media/lee/backup/xps13/
+    echo ""
+    echo "...complete"
+  else if test $hostname = 'alienware'
+     echo backing up alienware drive D: 
+    echo "this will take a few minutes..."
+    sleep 2
+    rsync -av /mnt/d/ /mnt/e/alienware/
+    echo ""
+    echo "...complete"
+
+  end
 
 end
