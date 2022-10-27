c1f860f2dfb3cd8fcbfe2886e55c24dcdd696401
lettuce <lettuce@future.home>
2022-10-27 13:52:48 -0400

    updating install stuff for void

---
+++ void-installer.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Switches keyboard to dvorak for this session
+setxkbmap dvorak
+
+echo "Updating packages list." \n
+sudo xbps-install -Su
+
+echo "Do it several times to update xbps as well..." \n
+sudo xbps-install -Su
+
+echo "Downloading vpm wrapper around xbps..." \n
+sudo xbps-install vpm
+
+echo "Adding non-free and muli-lib repos..."
+sudo vpm install void-repo-nonfree void-repo-multilib-nonfree
+
+# Installing my programs
+echo "Installing my default program (will fail and stop installing if a program is not found)..." \n
+sudo vpm install fish-shell neovim git mplayer pandoc sox w3m busybox cowsay espeak dialog ffmpeg figlet toilet gforth krita neofetch rhythmbox zenity bat ruby nnn curl ImageMagick lua luarocks pyradio tldr vlc yt-dlp love amfora lagrange firefox terminology
+
+# Switching default shell to fish
+echo "Changing default shell to fish..." \n
+chsh -s $(which fish)
+
+echo "Default shell changed to fish. Logout and back in or restart for changes to take effect..." \n
+
+# Copy config settings 
+echo "Copying over my config files..." \n
+sudo cp -r config/* ~/.config/
+
+# Fixing keyboard so it starts up dvorak
+echo "Adding dvorak keyboard startup file to X.org..." \n
+sudo mkdir /etc/X11/xorg.conf.d
+sudo cp /etc/X11/xorg.conf.d/dvorak /etc/X11/xorg.conf.d
+
+echo "You should now shutdown and restart your computer for changes to take full effect."
