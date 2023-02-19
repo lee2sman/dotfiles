efba382f47647d13967f092507edf7ca7129b2b7
lee2sman <leetusman@gmail.com>
2023-02-19 12:59:39 -0500

    add playdate sdk path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,9 @@
 set -gx EDITOR nvim
+
+export PLAYDATE_SDK_PATH=~/Games/playdate/PlaydateSDK-1.13.0
+
 status --is-interactive; and source (rbenv init -|psub)
 
 eval "$(luarocks path --bin)"
+
+fish_vi_key_bindings
