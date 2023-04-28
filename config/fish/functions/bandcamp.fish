0cbba0be7a8e617752e03393ca3b97c484e4c867
lee2sman <leetusman@gmail.com>
2023-04-28 15:06:47 -0400

    update bandcamp.fish to use yt-dlp instead of youtube-dl

--- config/fish/functions/bandcamp.fish
+++ config/fish/functions/bandcamp.fish
@@ -1,5 +1,5 @@
 function bandcamp --description 'stream bandcamp tracks of an album url'
 	echo "usage: bandcamp full-album-url"
 	echo "Navigate tracks with PgUp / PgDown"
-        mplayer (youtube-dl -g $argv)
+        mplayer (yt-dlp -g $argv)
 end
