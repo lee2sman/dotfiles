f133789ecb409525e77b379aab773e5050e3930e
lee2sman <leetusman@gmail.com>
2024-05-16 17:01:44 -0400

    improve open command.

--- config/fish/functions/open.fish
+++ config/fish/functions/open.fish
@@ -1,3 +1,7 @@
 function open --description 'simplify xdg-open command'
-	xdg-open $argv
+    if count $argv > /dev/null
+	xdg-open $argv & #open specified directory
+	else
+	  open . &     #else open here
+	end
 end
