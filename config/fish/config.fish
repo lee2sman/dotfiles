6dd774e0b759fc02fb6f59a3991c65d4480aee34
lee2sman <leetusman@gmail.com>
2025-07-16 23:09:51 +0200

    update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,5 +5,14 @@ end
 # set neovim editor default
 set -gx EDITOR nvim
 
+# set playdate sdk path
+set -gx PLAYDATE_SDK_PATH /home/void/Games/PlaydateSDK-2.1.0/
+
+# Set alsa as the audio backend for Love2d's OpenAL Soft audio library
+set -gx ALSOFT_DRIVERS alsa
+
 # turn on vi keybindings for fish repl
 fish_vi_key_bindings
+
+# Created by `pipx` on 2023-12-18 19:37:39
+set PATH $PATH /home/void/.local/bin
